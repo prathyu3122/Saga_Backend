519a52b9e4917ef2bf32ebefd9decef8d6dfe190
Prathyusha Rejeti <prathyusha.rejeti@ivycomptech.com>
2022-12-01 17:31:28 +0530

    Initial Commit

---
+++ src/main/java/com/example/SagaBackend/SagaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SagaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SagaBackend/SagaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SagaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
