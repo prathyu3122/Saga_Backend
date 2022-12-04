b11f6a355a95a1db6f51c67d7386aa923908bd67
Prathyusha Rejeti <prathyusha.rejeti@ivycomptech.com>
2022-12-04 12:40:16 +0530

    Author Controller is added and functionality is done

---
+++ src/main/java/com/example/SagaBackend/Controller/AuthorsController.java
@@ -0,0 +1,25 @@
+package com.example.SagaBackend.Controller;
+
+import com.example.SagaBackend.Model.Author;
+import com.example.SagaBackend.Model.Genre;
+import com.example.SagaBackend.Service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/saga")
+public class AuthorsController {
+    @Autowired
+    private AuthorService authorService;
+
+    @CrossOrigin
+    @GetMapping("/all-authors")
+    public List<Author> displayAllGenres() {
+        return authorService.displayAuthors();
+    }
+}
